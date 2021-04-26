60e98f27aaef54e25064f561c34e41e9cf554edf
heejicat <cheeji.choi@gmail.com>
2021-04-26 09:52:57 -0700

    fix helmet header

--- index.js
+++ index.js
@@ -31,19 +31,19 @@ app.use(express.static(path.join(__dirname, 'build')));
 
 app.use(helmet.contentSecurityPolicy({
     directives: {
-        defaultSrc: ["'self'"],
-        scriptSrc: ["'self'"],
-        styleSrc: ["'self'"],
-        imgSrc: ["'self'"],
-        connectSrc: ["'self'"],
-        fontSrc: ["'self'"],
-        objectSrc: ["'none'"],
-        mediaSrc: ["'self'"],
-        frameSrc: ["'none'"],
+        "defaultSrc": ["'self'"],
+        "scriptSrc": ["'self'"],
+        "styleSrc": ["'self'"],
+        "imgSrc": ["'self'"],
+        "connectSrc": ["'self'"],
+        "fontSrc": ["'self'"],
+        "objectSrc": ["'none'"],
+        "mediaSrc": ["'self'"],
+        "frameSrc": ["'none'"],
         // reportUri: '/report-violation',
-        reportOnly: false, // set to true if you only want to report errors
-        setAllHeaders: false, // set to true if you want to set all headers
-        safari5: false // set to true if you want to force buggy CSP in Safari 5   
+        "reportOnly": false, // set to true if you only want to report errors
+        "setAllHeaders": false, // set to true if you want to set all headers
+        "safari5": false // set to true if you want to force buggy CSP in Safari 5   
     }
 }));
 
