1117d1d85bc08610172f3a088b8162dcef174bb0
heejicat <cheeji.choi@gmail.com>
2021-04-26 13:43:42 -0700

    evn fix

--- client/src/API.js
+++ client/src/API.js
@@ -1,8 +1,8 @@
 const API_URL = process.env.API_URL;
 
 export async function covidData() {
+    console.log(API_URL);
     const response = await fetch(`${API_URL}/api/data`);
-    console.log(response);
     return response.json();
 }
 
