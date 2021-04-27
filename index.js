dad3d5b13dcbf3d427e293cdda70327ff1be4b98
heejicat <cheeji.choi@gmail.com>
2021-04-27 11:17:40 -0700

    typo fix

--- index.js
+++ index.js
@@ -16,12 +16,12 @@ const app = express();
 mongoose.connect(process.env.DATABASE_URL || 'mongodb://localhost/covid-19-dashboard', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-},
-err => { 
-    if (err) { 
-        console.error('database err', err); 
-    } else {
-        console.log("\x1b[35m", "Database is connected....")
+}, err => { 
+        if (err) { 
+            console.error('database err', err); 
+        } else {
+            console.log("\x1b[35m", "Database is connected....")
+        }
     }
 ); 
 
