da1dc13ee16aa6b160e6962e683b66a1855ee284
heejicat <cheeji.choi@gmail.com>
2021-04-30 17:38:18 -0700

    Fix Restriction page

--- client/src/NewRule.js
+++ client/src/NewRule.js
@@ -1,28 +1,30 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import axios from 'axios';
 
 function NewRule(_, ref) {
     const [rule, setRule] = useState();
 
-    const getRule = async () => {
-        await axios
-            .get('/api/rules')
-            .then( (rule) => {
-                // get new rule
-                const newRegHTML = rule.data[0].restriction;
+    useEffect(() => {
+        async function getRule() {
+            await axios
+                .get('/api/rules')
+                .then( (rule) => {
+                    // get new rule
+                    const newRegHTML = rule.data[0].restriction;
+    
+                    // const ruleTab = document.getElementById("rule");
+                    // const ruleDiv = document.createElement("div");
+                    // ruleTab.appendChild(ruleDiv);
+                    // ruleDiv.innerHTML = newReg;
+                    // setNewRule(newReg);
+    
+                    setRule(newRegHTML);
+                })
+                .catch( err => err); 
+        };
+        getRule();
+    }, []);
 
-                // const ruleTab = document.getElementById("rule");
-                // const ruleDiv = document.createElement("div");
-                // ruleTab.appendChild(ruleDiv);
-                // ruleDiv.innerHTML = newReg;
-                // setNewRule(newReg);
-
-                setRule(newRegHTML);
-            })
-            .catch( err => err); 
-    };
-
-console.log(getRule());
     return (
         <section ref={ref}>
             { rule }
