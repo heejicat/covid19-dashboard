8ec534788f859b4169c11802ea8d8db81a1c089e
heejicat <cheeji.choi@gmail.com>
2021-05-06 13:13:25 -0700

    Fix Restriction page

--- client/src/App.js
+++ client/src/App.js
@@ -35,7 +35,7 @@ function App() {
         .get('/api/rules')
         .then( (rule) => {
           // get new rule
-          const newRegHTML = rule.data[0].restriction;
+          let newRegHTML = rule.data[0].restriction;
           newRegHTML = newRegHTML.replace(/(?:\r\n|\r|\n|\t)/g, '');
                     console.log(newRegHTML);
           setNewRule({__html: DOMPurify.sanitize(newRegHTML)})
