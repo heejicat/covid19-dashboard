05ac7c059a9dd085e128f9494eb1bcd07844393c
heejicat <cheeji.choi@gmail.com>
2021-05-07 23:00:48 -0700

    add NivoChart

--- client/src/App.js
+++ client/src/App.js
@@ -21,11 +21,12 @@ function App() {
         // const dateFormat = {year: 'numeric', month: 'long', day: 'numeric' };
         // const date = new Date(data.data[0].date).toLocaleDateString("en-US", dateFormat);
         const date = new Date(data.data[0].date);
-        const today = today.setDate(date.getDate()+1);
+        const today = new Date(date);
+        today.setDate(date.getDate()+1);
         const todayCase = data.data[0].new_cases;
         
         setNewCase(todayCase);
-        setTodayDate(today);
+        setTodayDate(today.toDateString());
       })
       .catch( err => console.log(err));
   }
